03a4fe4244f93b58131d24ef203b4c53c2b21677
Tysheba <tmorgan2dev@gmail.com>
2018-11-24 21:41:27 -0500

    Updates

---
+++ precache-manifest.541964d98900cc874a82d987d1703bc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nytreactapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2c9e155529cf2b5d7f08",
+    "url": "/nytreactapp/static/js/runtime~main.2c9e1555.js"
+  },
+  {
+    "revision": "355cae3638b64210ff6d",
+    "url": "/nytreactapp/static/js/main.355cae36.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/nytreactapp/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "355cae3638b64210ff6d",
+    "url": "/nytreactapp/static/css/main.57ccff6c.chunk.css"
+  },
+  {
+    "revision": "a851c5340f12585609cae16aaecba125",
+    "url": "/nytreactapp/index.html"
+  }
+];
